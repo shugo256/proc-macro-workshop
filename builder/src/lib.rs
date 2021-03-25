cf8896f73ac3232eac7a86c774a134b88a4962ca
shugo256 <shugo256@gmail.com>
2021-03-25 18:50:41 +0900

    04-call-build

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,20 +1,21 @@
 use proc_macro::{TokenStream};
 use syn::{parse_macro_input, DeriveInput, Data, DataStruct, Fields, FieldsNamed, Field};
 use quote::{quote, format_ident};
-use proc_macro2::TokenStream as TokenStream2;
+use proc_macro2::{TokenStream as TokenStream2, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    let type_name = input.ident;
-    let builder_name = format_ident!("{}Builder", type_name);
+    let struct_name = input.ident;
+    let builder_name = format_ident!("{}Builder", struct_name);
 
     let fields = extract_fields(&input.data);
 
     let builder_fields = expand_builder_fields(&fields);
-    let init_fields = expand_init_fields(&fields);
+    let init_builder_fields = expand_init_builder_fields(&fields);
     let setters = expand_setters(&fields);
+    let build_method = expand_build_method(&fields, &struct_name);
 
     let expanded = quote! {
         pub struct #builder_name {
@@ -23,17 +24,17 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
         impl #builder_name {
             #setters
+            #build_method
         }
 
-        impl #type_name {
+        impl #struct_name {
             pub fn builder() -> #builder_name {
                 #builder_name {
-                    #init_fields
+                    #init_builder_fields
                 }
             }
         }
     };
-
     TokenStream::from(expanded)
 }
 
@@ -69,19 +70,35 @@ fn expand_builder_fields(fields: &FieldsNamed) -> TokenStream2 {
     })
 }
 
-fn expand_init_fields(fields: &FieldsNamed) -> TokenStream2 {
+fn expand_init_builder_fields(fields: &FieldsNamed) -> TokenStream2 {
     map_fields(fields, true, |field| {
         let ident = &field.ident;
         quote! { #ident: None }
     })
 }
 
+fn expand_build_method(fields: &FieldsNamed, struct_name: &Ident) -> TokenStream2 {
+    let init_struct_fields = map_fields(fields, true, |field| {
+        let ident = &field.ident;
+        let err_msg = format!("field {} missing!", ident.as_ref().unwrap());
+        quote! { #ident: self.#ident.clone().ok_or(#err_msg)? }
+    });
+    quote! {
+        pub fn build(&mut self) -> Result<#struct_name, Box<dyn std::error::Error>> {
+            Ok(#struct_name {
+                #init_struct_fields
+            })
+        }
+    }
+}
+
+
 fn expand_setters(fields: &FieldsNamed) -> TokenStream2 {
     map_fields(fields, false, |field| {
         let ident = &field.ident;
         let ty = &field.ty;
         quote! {
-            fn #ident(&mut self, #ident: #ty) -> &mut Self {
+            pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
                 self.#ident = Some(#ident);
                 self
             }
